1ee58697aebbe5d6e78197f238460758f2a18ad8
flintb30-oss <flintb30@smc.sa.edu.au>
2026-07-31 12:14:01 +0930

    Update Supabase URL in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://pmjujeqratksybfmilsw.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://pmjujeqratksybfmilsw.supabase.co";
 const SUPABASE_KEY = "sb_publishable_4raKS97uix90GfqxMwLz4w_mLdCbbV0";
 
 const supabaseClient = supabase.createClient(
